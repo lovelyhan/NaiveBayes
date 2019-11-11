7e527443b64b469d0f0dadc42a57cbdf7704c5fd
lovelyhan <598868984@qq.com>
2019-11-11 16:30:07 +0800

    modify master's code

--- src/main/scala/test/naivebayes/BayesApp.scala
+++ src/main/scala/test/naivebayes/BayesApp.scala
@@ -1,51 +1,21 @@
-package main.scala.test.naivebayes
+package test.naivebayes
 
-import org.apache.spark.mllib.linalg.{SparseVector, Vectors}
-import test.naivebayes.NativeBayesModel
+import org.apache.spark.{SparkConf, SparkContext}
 
 object BayesApp {
   def main(args: Array[String]): Unit = {
-    val nativeBayesModel = new NativeBayesModel()
-    val sc = nativeBayesModel.getSparkContext()
-    val partitionNum = sc.defaultMinPartitions
-    nativeBayesModel.initParams()
-    val path = "/user/hadoop/data/avazu/avazu-app"
-    val rdd = nativeBayesModel.loadData(sc, path)
-    val rddTotalNum = rdd.count()
-    for (epochTime <- 0 until nativeBayesModel.epoch) {
-      nativeBayesModel.logger.info(s"Epoch[${nativeBayesModel.epoch}] start training")
-      for (batchNum <- 0 until nativeBayesModel.batchNum) {
-        nativeBayesModel.logger.info(s"Iteration[$batchNum] starts")
-        val startBatchTime = System.currentTimeMillis()
-        val oneIterationRDD = rdd.sample(false, 0.05, 0L)
-        //广播全局参数
-        val broadcastMeans = sc.broadcast(nativeBayesModel.meanValues)
-        val broadcastVariance = sc.broadcast(nativeBayesModel.variance)
-        val total = oneIterationRDD.mapPartitions(miniList => nativeBayesModel.calculateMeansAndVariance(miniList, broadcastMeans.value, broadcastVariance.value))
-        nativeBayesModel.logger.debug(s"Mean value and variance are calculated,took${System.currentTimeMillis() - startBatchTime} ms")
-        //结果向量转矩阵
-        val sumAll = total.treeReduce((x, y) => {
-          val x1Sparse = x._1.asInstanceOf[SparseVector]
-          val x2Sparse = x._2.asInstanceOf[SparseVector]
-          val y1Sparse = y._1.asInstanceOf[SparseVector]
-          val y2Sparse = y._2.asInstanceOf[SparseVector]
-          nativeBayesModel.axpy(1,x1Sparse,y1Sparse)
-          nativeBayesModel.axpy(1,x2Sparse,y2Sparse)
-          (y1Sparse,y2Sparse)
-        })
-        val partitionMean = sumAll._1.toArray
-        for(j <- 0 to partitionMean.length){
-          partitionMean(j) = partitionMean(j)/partitionNum
-        }
-        val partitionVar = sumAll._2.toArray
-        for(k <- 0 to partitionVar.length){
-          partitionVar(k) = partitionVar(k) / partitionNum
-        }
-        nativeBayesModel.meanValues = Vectors.dense(partitionMean)
-        nativeBayesModel.variance = Vectors.dense(partitionVar)
-        nativeBayesModel.logger.debug(s"This term's calculation finished,totally took ${System.currentTimeMillis() - startBatchTime} ms")
-      }
-    }
-  }
+    // spark 的初始化放到外面来, 方便修改配置
+    val conf = new SparkConf()
+      .setAppName("Bayes Test")
+//      .setMaster("local")
+//      .set("spark.executor.memory","1g")
+    val sc = SparkContext.getOrCreate(conf)
 
+    // path 也抽出来, 方便使用的时候配置
+    val path = "/user/hadoop/data/avazu/avazu-app"
+    val model = new NativeBayesModel()
+    // 其它的可以修改的配置项也应该这样抽取出来作为参数传递进去
+    model.train(path, sc)
+    model.predict(path,sc)
+  }
 }
--- src/main/scala/test/naivebayes/NativeBayesModel.scala
+++ src/main/scala/test/naivebayes/NativeBayesModel.scala
@@ -1,33 +1,34 @@
 package test.naivebayes
 
-import java.util
-
-import org.apache.spark.mllib.linalg.{SparseVector, Vector, Vectors}
+import org.apache.spark.mllib.linalg.{DenseVector, SparseVector, Vector, Vectors}
 import org.apache.spark.mllib.regression.LabeledPoint
 import org.apache.spark.mllib.util.MLUtils
 import org.apache.spark.rdd.RDD
 import org.apache.spark.storage.StorageLevel
-import org.apache.spark.{SparkConf, SparkContext, SparkEnv}
+import org.apache.spark.{SparkConf, SparkContext}
 import org.slf4j.{Logger, LoggerFactory}
+import test.naivebayes.NativeBayesModel.Model.{meanValue1, meanValues0, variance0, variance1}
 
-import scala.collection.mutable.{ArrayBuffer, ListBuffer}
+import scala.collection.mutable.ArrayBuffer
 
+object NativeBayesModel {
+  private val logger: Logger = LoggerFactory.getLogger(NativeBayesModel.getClass)
+
+  object Model {
+    //Gaussian Bayes model
+    var meanValues0: Vector = _
+    var meanValue1: Vector = _
+    var variance0: Vector = _
+    var variance1: Vector = _
+  }
+}
+
+import test.naivebayes.NativeBayesModel.Model._
 class NativeBayesModel() extends Serializable{
-  val logger: Logger = LoggerFactory.getLogger(this.getClass)
+  @transient protected val logger: Logger = NativeBayesModel.logger
   final val dimensions = 999990
   val epoch = 1
   val batchNum = 20
-  var meanValues: Vector = _
-  var variance: Vector = _
-
-    def getSparkContext(): SparkContext = {
-      val conf = new SparkConf()
-        .setAppName("Bayes Test")
-//        .setMaster("local")
-//        .set("spark.executor.memory","1g")
-      val sc = SparkContext.getOrCreate(conf)
-      sc
-    }
 
     def loadData(sc: SparkContext,path: String):RDD[LabeledPoint] = {
       val startTime = System.currentTimeMillis()
@@ -36,103 +37,187 @@ class NativeBayesModel() extends Serializable{
         .persist(StorageLevel.MEMORY_AND_DISK)
       val totalLength = dataRdd.count()
       logger.info(s"Load data cost ${System.currentTimeMillis() - startTime} ms")
+      println(s"Load data cost ${System.currentTimeMillis() - startTime} ms")
       dataRdd
     }
 
-  def initParams(): Unit ={
-      val means = new Array[Double](dimensions)
-      meanValues = Vectors.dense(means)
-      val variances = new Array[Double](dimensions)
-      variance = Vectors.dense(variances)
+    protected def initParams(): Unit ={
+      val means1,means2 = new Array[Double](dimensions)
+      meanValues0 = Vectors.dense(means1)
+      meanValue1 = Vectors.dense(means2)
+      val var1,var2 = new Array[Double](dimensions)
+      variance0 = Vectors.dense(var1)
+      variance1 = Vectors.dense(var2)
     }
 
-  def calculateMeansAndVariance(miniBatch: Iterator[LabeledPoint],totalMeans: Vector,totalVariances: Vector): Iterator[Tuple2[Vector,Vector]] ={
-    val ans = new ArrayBuffer[Tuple2[Vector,Vector]]
-    val totalMeans = new Array[Double](dimensions)
-    val totalVar = new Array[Double](dimensions)
+  def calculateMeansAndVariance(miniBatch: Iterator[LabeledPoint],totalMeans0: Vector,totalMeans1: Vector,totalVariance0: Vector,totalVariance1: Vector): Iterator[Tuple4[Array[Double],Array[Double],Array[Double],Array[Double]]] ={
+    val ans = new ArrayBuffer[Tuple4[Array[Double],Array[Double],Array[Double],Array[Double]]]
+    val totalMeans0 = new Array[Double](dimensions)
+    val totalMeans1 = new Array[Double](dimensions)
+    val totalVar0 = new Array[Double](dimensions)
+    val totalVar1 = new Array[Double](dimensions)
+    var totalCount0 = 0
+    var totalCount1 = 0
     val data = miniBatch.toList
     val dataLength = data.length
 
     for(m <- 0 until data.length) {
       val labeledPoint = data.apply(m)
+      val label = labeledPoint.label
       val features = labeledPoint.features.asInstanceOf[SparseVector]
       val keys = features.indices
       val values = features.values
       //Sum up all to calculate mean value
       for (i <- 0 until keys.length) {
         val num = keys.apply(i)
-        totalMeans(num) += values.apply(i)
+        if(label == 0.0){
+          totalMeans0(num) += values.apply(i)
+          totalCount0 += 1
+        }
+        else {
+          totalMeans1(num) += values.apply(i)
+          totalCount1 += 1
+        }
       }
     }
     for(m <- 0 until dimensions){
-      totalMeans(m) /= dataLength
+      totalMeans0(m) /= totalCount0
+      totalMeans1(m) /= totalCount1
     }
     for(n <- 0 until data.length){
       val labeledPoint = data.apply(n)
       val features = labeledPoint.features.asInstanceOf[SparseVector]
+      val label = labeledPoint.label
       val keys = features.indices
       val values = features.values
       for(k <- 0 until keys.length){
         val num = keys.apply(k)
-        val diff = values.apply(k) - totalMeans.apply(num)
-        totalVar(num) += diff * diff
+        val diff = values.apply(k) - totalMeans0(num)
+        if(label == 0.0){
+          totalVar0(num) += diff * diff
+        }
+        else{
+          totalVar1(num) += diff * diff
+        }
       }
     }
-    ans.+=:(totalMeans.asInstanceOf[Vector],totalVar.asInstanceOf[Vector])
+    ans.+=:(totalMeans0,totalMeans1,totalVar0,totalVar1)
    ans.iterator
   }
 
-  def axpy(a: Int,x: SparseVector,y:SparseVector):Unit ={
-    val xIndexs = x.indices
-    val xlength = xIndexs.length
-    val xValues = x.values
-    val yValues = y.values
-    var k = 0
-    while(k < xlength){
-      yValues(xIndexs(k)) += a * xValues(k)
-      k += 1
+  def axpy(a: Double, x: Array[Double], y: Array[Double]): Unit = {
+    for(i <- 0 to y.length - 1){
+      y(i) += a * x(i)
     }
   }
-//  def main(args: Array[String]): Unit = {
-//    val sc = getSparkContext()
-//    val partitionNum = sc.defaultMinPartitions
-//    initParams()
-//    val path = "/user/hadoop/data/avazu/avazu-app"
-//    val rdd = loadData(sc, path)
-//    val rddTotalNum = rdd.count()
-//    for (epochTime <- 0 until epoch) {
-//      logger.info(s"Epoch[$epoch] start training")
-//      for (batchNum <- 0 until batchNum) {
-//        logger.info(s"Iteration[$batchNum] starts")
-//        val startBatchTime = System.currentTimeMillis()
-//        val oneIterationRDD = rdd.sample(false, 0.05, 0L)
-//        //广播全局参数
-//        val broadcastMeans = sc.broadcast(meanValues)
-//        val broadcastVariance = sc.broadcast(variance)
-//        val total = oneIterationRDD.mapPartitions(miniList => calculateMeansAndVariance(miniList, broadcastMeans.value, broadcastVariance.value))
-//        logger.debug(s"Mean value and variance are calculated,took${System.currentTimeMillis() - startBatchTime} ms")
-//        //结果向量转矩阵
-//        val sumAll = total.treeReduce((x, y) => {
-//          val x1Sparse = x._1.asInstanceOf[SparseVector]
-//          val x2Sparse = x._2.asInstanceOf[SparseVector]
-//          val y1Sparse = y._1.asInstanceOf[SparseVector]
-//          val y2Sparse = y._2.asInstanceOf[SparseVector]
-//          axpy(1,x1Sparse,y1Sparse)
-//          axpy(1,x2Sparse,y2Sparse)
-//          (y1Sparse,y2Sparse)
-//        })
-//        val partitionMean = sumAll._1.toArray
-//        for(j <- 0 to partitionMean.length){
-//          partitionMean(j) = partitionMean(j)/partitionNum
-//        }
-//        val partitionVar = sumAll._2.toArray
-//        for(k <- 0 to partitionVar.length){
-//          partitionVar(k) = partitionVar(k) / partitionNum
-//        }
-//        meanValues = Vectors.dense(partitionMean)
-//        variance = Vectors.dense(partitionVar)
-//        logger.debug(s"This term's calculation finished,totally took ${System.currentTimeMillis() - startBatchTime} ms")
-//      }
-//    }
-//  }
+
+  def train(path: String, sc: SparkContext): Unit = {
+    val partitionNum = sc.defaultMinPartitions
+    initParams()
+    val rdd = loadData(sc, path)
+    val rddTotalNum = rdd.count()
+    val ansVec = new Array[Vector](4)
+    for (epochTime <- 0 until epoch) {
+      logger.info(s"Epoch[$epoch] start training")
+      println(s"Epoch[$epoch] start training")
+      for (batchNum <- 0 until batchNum) {
+        logger.info(s"Iteration[$batchNum] starts")
+        println(s"Iteration[$batchNum] starts")
+        val startBatchTime = System.currentTimeMillis()
+        val oneIterationRDD = rdd.sample(false, 0.05, 0L)
+        //广播全局参数
+        val broadcastMeans0 = sc.broadcast(meanValues0)
+        val broadcastVariance0 = sc.broadcast(variance0)
+        val broadcastMeans1 = sc.broadcast(meanValue1)
+        val broadcastVariance1 = sc.broadcast(variance1)
+        val total = oneIterationRDD.mapPartitions(miniList => calculateMeansAndVariance(miniList, broadcastMeans0.value, broadcastMeans1.value,broadcastVariance0.value,broadcastVariance1.value))
+        logger.info(s"Mean value and variance are calculated,took${System.currentTimeMillis() - startBatchTime} ms")
+        println(s"Mean value and variance are calculated,took${System.currentTimeMillis() - startBatchTime} ms")
+        val sumAll = total.treeReduce((x,y) => {
+          axpy(1.0,x._1,y._1)
+          axpy(1.0,x._2,y._2)
+          axpy(1.0,x._3,y._3)
+          axpy(1.0,x._4,y._4)
+          y
+        })
+        val partitionMean0 = sumAll._1
+        for(j <- 0 to partitionMean0.length - 1){
+          partitionMean0(j) = partitionMean0(j)/partitionNum
+        }
+        val partitionMean1 = sumAll._2
+        for(k <- 0 to partitionMean1.length - 1){
+          partitionMean1(k) = partitionMean1(k) / partitionNum
+        }
+        val partitionVar0 = sumAll._3
+        for(j <- 0 to partitionVar0.length - 1){
+          partitionVar0(j) = partitionVar0(j)/partitionNum
+        }
+        val partitionVar1 = sumAll._4
+        for(j <- 0 to partitionVar1.length - 1){
+          partitionVar1(j) = partitionVar1(j)/partitionNum
+        }
+        meanValues0 = Vectors.dense(partitionMean0)
+        meanValue1 = Vectors.dense(partitionMean1)
+        variance0 = Vectors.dense(partitionVar0)
+        variance1 = Vectors.dense(partitionVar1)
+
+        logger.info(s"This term's calculation finished,totally took ${System.currentTimeMillis() - startBatchTime} ms")
+        println(s"This term's calculation finished,totally took ${System.currentTimeMillis() - startBatchTime} ms")
+      }
+    }
+    ansVec
+  }
+
+  //预测
+  def predict(path: String, sc: SparkContext):Unit = {
+    val rdd = loadData(sc, path)
+    val predictRdd = rdd.sample(false,0.01,0)
+    val rddTotalNum = predictRdd.count()
+    var right = 0
+
+    val ans = predictRdd.map(x =>{
+      //whetherTrue is used to compare whethre the prediction of our model
+      //is equivalent to the original label
+      val whetherTrue = 0.0
+      val label = x.label
+      var pre0 = 1.0
+      var pre1 = 1.0
+      val features = x.features.asInstanceOf[SparseVector]
+      val keys = features.indices
+      val values = features.values
+      //Sum up all to calculate mean value
+      for (i <- 0 until keys.length) {
+        //get the number of the feature
+        val num = keys.apply(i)
+        val xValue = values.apply(i)
+        //prepare to predict
+        val mean_0 = meanValues0.apply(num)
+        val mean_1 = meanValue1.apply(num)
+        val var_0 = variance0.apply(num)
+        val var_1 = variance1.apply(num)
+        var predict0 = (-1.0) * (xValue - mean_0) * (xValue - mean_0) / (2 * var_0)
+        predict0 = math.exp(predict0)
+        predict0 = (1.0) / math.sqrt(2 * math.Pi * var_0) * predict0
+        var predict1 = (-1.0) * (xValue - mean_1) * (xValue - mean_1) / (2 * var_1)
+        predict1 = math.exp(predict1)
+        predict1 = (1.0) / math.sqrt(2 * math.Pi * var_1) * predict1
+        pre1 *= predict0
+      }
+      //compare the value of prediction
+      var finalSelect = 0.0
+      if(pre1 >= pre0){
+        finalSelect = 1.0
+      }
+      if (finalSelect == label){
+        finalSelect = 1.0
+      }
+      finalSelect
+    })
+    val finalTotal = ans.reduce((x,y) => {
+     x+y
+    })
+    val rate = 1.0 * finalTotal / rddTotalNum
+    logger.info(s"The accuracy of model is ${rate}")
+    println(s"The accuracy of model is ${rate}")
+  }
 }
